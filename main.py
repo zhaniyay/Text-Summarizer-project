4bafa52a8011665f4cf7cee82cf1139998d1fe19
MacBook Pro <zhaniyayeleussiz@gmail.com>
2025-02-27 23:34:52 +0500

    data validation completed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-#from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 #from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 #from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 #from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
@@ -17,8 +17,15 @@ except Exception as e:
         raise e
 
 
-
-
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
