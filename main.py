2c26e3a9d1881a33bdabc992dc2b7c3c0dff9d29
rymqan <alisher.rymkan@gmail.com>
2025-02-28 21:17:26 +0500

    prediction & UI added

--- main.py
+++ main.py
@@ -40,15 +40,17 @@ except Exception as e:
         raise e
 
 
-#STAGE_NAME = "Model Trainer stage"
-#try: 
- # logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-  # model_trainer = ModelTrainerTrainingPipeline()
-   #model_trainer.main()
-  # logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-#except Exception as e:
-   #     logger.exception(e)
-    #    raise e
+STAGE_NAME = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
 
 
 
